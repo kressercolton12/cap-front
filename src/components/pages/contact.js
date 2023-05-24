d6b1249a8074ddd75fd7b3d9dc63255bfd8d71fd
Colton Kresser <kressercolton12@gmail.com>
2023-05-24 09:43:37 -0600

    Updates

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,27 +1,45 @@
 import React from 'react';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import contactPhoto from "../../../static/assets/images/DSC03228.jpg";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function() {
     return (
-        <div className='page-content'>
-        <div className='left'
+        <div className='content'>
+        <div className='left-content'
         style={{
             background: "url(" + contactPhoto + ") no-repeat",
             backgroundSize: "cover",
             backgroundPosition: "center"
         }}
         />
-        <div className='right'>
+        <div className='right-content'>
             <div className='bullet-points'>
                 <div className='bullet-group'>
-                    <div className='icons'>
-                        {/* <FontAwesomeIcon icon='' */}
-
+                    <div className='icon'>
+                        <FontAwesomeIcon icon={faPhone} />
                     </div>
-
+                    <div className='phone-number'>801-458-4124</div>
                 </div>
+
+                <div className='bullet-group'>
+                    <div className='icon'>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                    </div>
+                    <div className='email'>jessica@unfinishedlifestyle.com</div>
+                </div>
+
+                <div className='bullet-group'>
+                    <div className='icon'>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </div>
+                    <div className="text"><a target="_blank" href="https://www.facebook.com/Unfinishedlifestyle">Facebook</a>  <FontAwesomeIcon icon={faLink} /></div>
+                </div>
+
             </div>
 
         </div>
